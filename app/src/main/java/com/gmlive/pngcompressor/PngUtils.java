47ec4428a5866344f0d1c5c407a3e0cd99468a8e
ouyangshaocong <ouyangshaocong@inke.cn>
2020-07-27 12:11:16 +0800

    修复代码

--- app/src/main/java/com/gmlive/pngcompressor/PngUtils.java
+++ app/src/main/java/com/gmlive/pngcompressor/PngUtils.java
@@ -1,39 +1,15 @@
 package com.gmlive.pngcompressor;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 
 import androidx.annotation.NonNull;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-
-import static android.graphics.Bitmap.CompressFormat.PNG;
 
 class PngUtils {
 
-	private PngUtils() {
-	}
-
-	static File createTestPngFile(@NonNull Context context) {
-		Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.goldfish);
-		File file = getTempFile(context);
-
-		try (OutputStream outputStream = new FileOutputStream(file)) {
-			bitmap.compress(PNG, 100, outputStream);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		return file;
-	}
-
-	static File getTempFile(@NonNull Context context) {
-		return getTempFile(context, "temp", ".png");
-	}
+	private PngUtils() { }
 
 	static File getTempFile(@NonNull Context context, String name, String suffix) {
 		try {
